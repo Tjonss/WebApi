882a1dd8834f897898d70f479c63f4110a86fa7f
Jonas <jonasengstrom_@hotmail.com>
2022-10-31 10:41:46 +0100

    asdsa

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import { useRef } from 'react'
 
 const Create = () => {
+
+  const titleRef = useRef()
+  const descRef = useRef()
+  const emailRef = useRef()
+
+
+
+  const onSubmit = e => {
+    e.preventDefault()
+
+    console.log({title: titleRef.current.value, desc: descRef.current.value, email: emailRef.current.value})
+  }
+  
+
+ 
 
   return (
     <>
     <h2 className='text-center mt-3 Create'>Create new case</h2>
-      <form className='container mt-5'>
+      <form onSubmit={onSubmit} className='container mt-5'>
         <div className='form-floating'>
-            <input type='text' className='form-control' id='floatingInputGrid' placeholder='name@example.com' />
+            <input 
+              type='text' 
+              className='form-control' 
+              id='floatingInputGrid' 
+              placeholder='name@example.com'
+              ref={titleRef} />
             <label htmlFor='floatingInputGrid'>Enter a title..</label>
         </div>
         <div className='form-floating mt-4'>
-          <textarea style={styles.textarea} className='form-control custom-textarea' placeholder='Leave a comment here' id='floatingTextarea2' ></textarea>
+          <textarea style={styles.textarea} ref={descRef} className='form-control custom-textarea' placeholder='Leave a comment here' id='floatingTextarea2' ></textarea>
           <label htmlFor='floatingTextarea2'>Description</label>
         </div>
-
         <div className='row g-2 mt-3'>
           <div className='col-md'>
             <div className='form-floating'>
-              <input type='email' className='form-control' id='floatingInputGrid' placeholder='name@example.com' />
+              <input 
+                type='email' 
+                className='form-control' 
+                id='floatingInputGrid' 
+                placeholder='name@example.com'
+                ref={emailRef} />
               <label htmlFor='floatingInputGrid'>Email address</label>
             </div>
           </div>
@@ -27,7 +51,6 @@ const Create = () => {
               <select className='form-select' id='floatingSelectGrid'>
                 <option>Not opened</option>
                 <option>Opened</option>
-                <option>Finished</option>
               </select>
               <label htmlFor='floatingSelectGrid'>Status</label>
             </div>
--- src/store/actiontypes.js
+++
@@ -1,5 +0,0 @@
-const actiontypes = () => {
-  
-}
-
-export default actiontypes
